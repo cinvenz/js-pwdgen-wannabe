86a322a7338d688fd310087cb6a648f6b3da0fc5
cinvenz <vincenzobortune@gmail.com>
2022-10-19 17:19:51 +0200

    commit finale

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let nome = prompt("Qual'è il tuo nome")
+let cognome = prompt("Qual è il tuo cognome?")
+let colorePreferito = prompt("Qual' è il tuo colore preferito?")
+
+
+
+document.getElementById('name').innerHTML = nome
+document.getElementById('surname').innerHTML = cognome
+document.getElementById('color').innerHTML = colorePreferito
+document.getElementById('nomecognomecolorepreferito22').innerHTML = 'nomecognomecolorepreferito22'
